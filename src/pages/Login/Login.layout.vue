e190ab0b006b7d95eebba6e03f9a5b02b0cf0097
Victor P <katsukeinz@gmail.com>
2022-04-20 01:38:00 -0400

    created login logic

---
+++ src/pages/Login/Login.layout.vue
@@ -0,0 +1,89 @@
+<template>
+  <q-form
+    @submit="() => onSubmit({ email: emailValue, password: passwordValue })"
+    class="q-gutter-md"
+  >
+    <q-input
+      filled
+      :label="emailAttrs.label"
+      :hint="emailAttrs.hint"
+      :type="emailAttrs.type"
+      :model-value="emailValue"
+      @update:model-value="$emit('update:emailValue', $event)"
+      lazy-rules
+      :rules="emailValidations"
+    />
+
+    <q-input
+      filled
+      :label="passwordAttrs.label"
+      :hint="passwordAttrs.hint"
+      :type="passwordAttrs.type"
+      :model-value="passwordValue"
+      @update:model-value="$emit('update:passwordValue', $event)"
+      lazy-rules
+      :rules="passwordValidations"
+    />
+
+    {{ emailValue }}
+
+    <q-toggle v-model="accept" label="I accept the license and terms" />
+
+    <div>
+      <q-btn label="Submit" type="submit" color="primary" />
+      <q-btn label="Reset" type="reset" color="primary" flat class="q-ml-sm" />
+    </div>
+  </q-form>
+</template>
+
+<script>
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  props: {
+    onSubmit: {
+      type: Function,
+      default: () => {
+        return {};
+      },
+    },
+    emailValue: {
+      type: String,
+      default: '',
+    },
+    emailAttrs: {
+      type: Object,
+      default: () => {
+        return {
+          label: 'Your email',
+          hint: 'example@example.com',
+          type: 'text',
+        };
+      },
+    },
+    passwordValue: {
+      type: String,
+      default: '',
+    },
+    passwordAttrs: {
+      type: Object,
+      default: () => {
+        return {
+          label: 'Your password',
+          hint: '',
+          type: 'password',
+        };
+      },
+    },
+    emailValidations: {
+      type: Array,
+      default: () => [],
+    },
+    passwordValidations: {
+      type: Array,
+      default: () => [],
+    },
+  },
+  emits: ['update:emailValue', 'update:passwordValue'],
+});
+</script>
--- src/pages/Login/index.vue
+++ src/pages/Login/index.vue
@@ -1,11 +1,60 @@
 <template>
-  <div>Login Page</div>
+  <div class="login">
+    <login-layout
+      v-model:email-value="userEmail"
+      v-model:password-value="userPassword"
+      :passwordValidations="passwordValidations"
+      :emailValidations="emailValidations"
+      :onSubmit="onSubmit"
+    />
+
+    {{ loginResult }}
+    {{ loginState }}
+  </div>
 </template>
 
 <script lang="ts">
-import { defineComponent } from 'vue';
+import { defineComponent, ref, watch } from 'vue';
+import './Login.css';
+import { handleUserLogin, userAuthData } from '../../actions';
+import { emailValidations, passwordValidations } from '../../utils/validations';
+import LoginLayout from './Login.layout.vue';
 
 export default defineComponent({
   name: 'Login',
+  components: {
+    LoginLayout,
+  },
+  setup: async () => {
+    const userEmail = ref<string>('');
+    const userPassword = ref<string>('');
+    const loginResult = ref<object>({});
+    const loginIsLoading = ref<boolean>(false);
+
+    const logUser = ({ email, password }: userAuthData) => {
+      loginIsLoading.value = true;
+
+      handleUserLogin({ email, password })
+        .then((res) => {
+          console.log(res);
+        })
+        .catch((err) => {
+          console.error(err);
+        })
+        .finally(() => {
+          loginIsLoading.value = false;
+        });
+    };
+
+    return {
+      onSubmit: logUser,
+      userEmail,
+      userPassword,
+      emailValidations,
+      passwordValidations,
+      loginResult,
+      loginIsLoading,
+    };
+  },
 });
 </script>
