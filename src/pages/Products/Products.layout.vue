897dfc6faad6463c588d4dc82c49c36f9e19d51c
Victor P <katsukeinz@gmail.com>
2022-04-21 16:37:36 -0400

    Created Products layout

---
+++ src/pages/Products/Products.layout.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="ProductsLayout">
+    <div class="Products__filters">
+      <search-bar
+        @on-search="$emit('onSearch')"
+        @on-clear="$emit('onClear')"
+        @update:query-value="$emit('update:queryValue', $event)"
+        :query-value="queryValue"
+      />
+      <q-select
+        filled
+        v-model="modelSingle"
+        :options="options"
+        use-chips
+        stack-label
+        label="Single selection"
+      />
+    </div>
+
+    <product-list :isLoading="isLoading" :products="products" />
+
+    <div class="q-pa-lg flex flex-center">
+      <page-pagination
+        @onChangePage="$emit('onChangePage', $event)"
+        :currentPage="currentPage"
+        :totalPages="totalPages"
+      />
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+import ProductList from './ProductList.vue';
+import SearchBar from '../../components/SearchBar/';
+import PagePagination from '../../components/PagePagination.vue';
+
+export default defineComponent({
+  name: 'ProductsLayout',
+  components: {
+    PagePagination,
+    ProductList,
+    SearchBar,
+  },
+  props: {
+    products: {
+      type: Array,
+      default: () => [],
+    },
+    queryValue: {
+      type: String,
+      default: '',
+    },
+    totalProducts: {
+      type: Number,
+      default: 0,
+    },
+    totalPages: {
+      type: Number,
+      default: 1,
+    },
+    currentPage: {
+      type: Number,
+      default: 1,
+    },
+    isLoading: {
+      type: Boolean,
+      default: false,
+    },
+  },
+  emits: ['update:queryValue', 'onChangePage', 'onSearch', 'onClear'],
+});
+</script>
