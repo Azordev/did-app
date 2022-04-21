965f1e21df2f0df292dabf20fdf46477f9dbaf4c
Victor P <katsukeinz@gmail.com>
2022-04-21 16:34:45 -0400

    Created page pagination and search bar components

---
+++ src/components/PagePagination.vue
@@ -0,0 +1,29 @@
+<template>
+  <q-pagination
+    :model-value="currentPage"
+    @update:model-value="$emit('onChangePage', $event)"
+    color="black"
+    :max="totalPages"
+    :max-pages="5"
+    :boundary-numbers="false"
+  />
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'ProductSearchBar',
+  props: {
+    currentPage: {
+      type: Number,
+      default: 1,
+    },
+    totalPages: {
+      type: Number,
+      default: 1,
+    },
+  },
+  emit: ['onChangePage'],
+});
+</script>
---
+++ src/components/SearchBar/SearchBar.vue
@@ -0,0 +1,38 @@
+<template>
+  <q-form @submit="$emit('onSearch')">
+    <q-input
+      outlined
+      bottom-slots
+      :model-value="queryValue"
+      @update:model-value="$emit('update:queryValue', $event)"
+      label="Busqueda"
+    >
+      <template v-slot:append>
+        <q-icon
+          v-if="queryValue !== ''"
+          name="close"
+          @click="$emit('onClear')"
+          class="cursor-pointer"
+        />
+        <q-icon class="SearchBar__button" name="search" type="submit" />
+      </template>
+
+      <template v-slot:hint> Field hint </template>
+    </q-input>
+  </q-form>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'SearchBar',
+  props: {
+    queryValue: {
+      type: String,
+      default: '',
+    },
+  },
+  emit: ['update:queryValue', 'onClear'],
+});
+</script>
