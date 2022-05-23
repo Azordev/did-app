879498a350be761db7e0ce77396e9274d04b503d
Victor P <katsukeinz@gmail.com>
2022-05-22 23:15:36 -0400

    Some fixes to back button component

--- src/components/BackButton/BackButton.vue
+++ src/components/BackButton/BackButton.vue
@@ -5,60 +5,28 @@
     dense
     size="md"
     :icon="arrowIcon"
-    :label="backButtonText"
+    :label="toHome ? 'Inicio' : 'Atrás'"
     no-caps
+    @click="toHome ? $router.push({ name: 'dashboard' }) : $router.back()"
   />
 </template>
 
 <script lang="ts">
-import { defineComponent, ref } from 'vue';
+import { defineComponent } from 'vue';
 import { matArrowBackIosNew } from '@quasar/extras/material-icons';
-import { useRouter } from 'vue-router';
-
-const getButtonText = () => {
-  const router = useRouter();
-  const backButtonText = ref<string>('Atrás');
-
-  const homeRoute = router.resolve({ name: 'dashboard' }).fullPath;
-  const previousRoute = window.history.state.back;
-
-  if (homeRoute === previousRoute) {
-    backButtonText.value = 'Inicio';
-  }
-
-  return { backButtonText };
-};
 
 export default defineComponent({
   name: 'SearchBar',
   emits: ['onChange'],
   props: {
-    value: {
-      type: String,
-      default: '',
-    },
-    options: {
-      type: Array,
-      default: () => [],
-    },
-    optionLabel: {
-      type: String,
-      default: null,
-    },
-    optionValue: {
-      type: String,
-      default: null,
-    },
-    label: {
-      type: String,
-      default: 'Selecciona una opcion',
+    toHome: {
+      type: Boolean,
+      default: false,
     },
   },
   emit: ['update:queryValue', 'onClear'],
   setup() {
-    const { backButtonText } = getButtonText();
-
-    return { arrowIcon: matArrowBackIosNew, backButtonText };
+    return { arrowIcon: matArrowBackIosNew };
   },
 });
 </script>
