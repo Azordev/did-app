1cf2c7a5c426eca60274f7d9dd68d5ce115b779e
Victor P <katsukeinz@gmail.com>
2022-06-04 20:53:47 -0400

    Exported links to a component and adding active link effect

--- src/layouts/MainLayout/MainLayout.vue
+++ src/layouts/MainLayout/MainLayout.vue
@@ -8,18 +8,21 @@
 
     <nav bordered class="navbar__container bg-white text-black">
       <div class="navbar">
-        <q-btn class="navbar__button" :to="{ name: 'dashboard' }">
-          <home-icon />
-          <span class="navbar__text">Inicio</span>
-        </q-btn>
-        <q-btn class="navbar__button" :to="{ name: 'events' }">
-          <events-icon />
-          <span class="navbar__text">Eventos</span>
-        </q-btn>
-        <q-btn class="navbar__button" :to="{ name: 'providers' }">
-          <providers-icon />
-          <span class="navbar__text">Proveedores</span>
-        </q-btn>
+        <app-link :to="{ name: 'dashboard' }" title="Inicio">
+          <template #icon>
+            <home-icon />
+          </template>
+        </app-link>
+        <app-link :to="{ name: 'events' }" title="Eventos">
+          <template #icon>
+            <events-icon />
+          </template>
+        </app-link>
+        <app-link :to="{ name: 'providers' }" title="Proveedores">
+          <template #icon>
+            <providers-icon />
+          </template>
+        </app-link>
       </div>
     </nav>
   </div>
@@ -28,9 +31,8 @@
 <script lang="ts">
 import { defineComponent } from 'vue';
 import { eventsIcon, homeIcon, providersIcon } from './assets';
+import AppLink from './components/AppLink.vue';
 import './styles.scss';
-
-console.log(eventsIcon);
 
 export default defineComponent({
   name: 'MainLayout',
@@ -38,6 +40,7 @@ export default defineComponent({
     eventsIcon,
     homeIcon,
     providersIcon,
+    AppLink,
   },
 });
 </script>
---
+++ src/layouts/MainLayout/components/AppLink.vue
@@ -0,0 +1,37 @@
+<template>
+  <router-link
+    :to="{ name: 'dashboard' }"
+    custom
+    v-slot="{ isExactActive, navigate }"
+    exact
+  >
+    <q-btn
+      :class="`navbar__button ${isExactActive ? 'navbar__button--active' : ''}`"
+      @click="navigate"
+      flat
+    >
+      <slot name="icon"></slot>
+      <span class="navbar__text">Inicio</span>
+    </q-btn>
+  </router-link>
+</template>
+
+<script lang="ts">
+import { PropType } from '@vue/runtime-core';
+import { RouteLocationRaw } from 'vue-router';
+
+export default {
+  name: 'AppLink',
+  props: {
+    to: {
+      type: Object as PropType<RouteLocationRaw>,
+    },
+    title: {
+      type: String,
+      default: '',
+    },
+  },
+};
+</script>
+
+<style></style>
