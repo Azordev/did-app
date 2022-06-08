3bcb262ada7d08b926b42eec8b80d423d731926a
Victor P <katsukeinz@gmail.com>
2022-06-08 19:21:05 -0400

    Ranning yarn test

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,22 +8,28 @@ module.exports = {
   // `parser: 'vue-eslint-parser'` is already included with any 'plugin:vue/**' config and should be omitted
   parserOptions: {
     parser: require.resolve('@typescript-eslint/parser'),
-    extraFileExtensions: ['.vue']
+    extraFileExtensions: ['.vue'],
   },
   env: {
     browser: true,
     es2021: true,
     node: true,
-    'vue/setup-compiler-macros': true
+    'vue/setup-compiler-macros': true,
   },
   // Rules order is important, please avoid shuffling them
-  extends: ['plugin:@typescript-eslint/recommended', 'plugin:vue/vue3-essential', 'prettier', 'plugin:storybook/recommended'],
-  plugins: [// required to apply rules which need type information
-  '@typescript-eslint', // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-files
-  // required to lint *.vue files
-  'vue' // https://github.com/typescript-eslint/typescript-eslint/issues/389#issuecomment-509292674
-  // Prettier has not been included as plugin to avoid performance impact
-  // add it as an extension for your IDE
+  extends: [
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-essential',
+    'prettier',
+    'plugin:storybook/recommended',
+  ],
+  plugins: [
+    // required to apply rules which need type information
+    '@typescript-eslint', // https://eslint.vuejs.org/user-guide/#why-doesn-t-it-work-on-vue-files
+    // required to lint *.vue files
+    'vue', // https://github.com/typescript-eslint/typescript-eslint/issues/389#issuecomment-509292674
+    // Prettier has not been included as plugin to avoid performance impact
+    // add it as an extension for your IDE
   ],
   globals: {
     ga: 'readonly',
@@ -36,14 +42,18 @@ module.exports = {
     __QUASAR_SSR_PWA__: 'readonly',
     process: 'readonly',
     Capacitor: 'readonly',
-    chrome: 'readonly'
+    chrome: 'readonly',
   },
   // add your custom rules here
   rules: {
     'prefer-promise-reject-errors': 'off',
-    quotes: ['warn', 'single', {
-      avoidEscape: true
-    }],
+    quotes: [
+      'warn',
+      'single',
+      {
+        avoidEscape: true,
+      },
+    ],
     // this rule, if on, would require explicit return type on the `render` function
     '@typescript-eslint/explicit-function-return-type': 'off',
     // in plain CommonJS modules, you can't use `import foo = require('foo')` to pass this rule, so it has to be disabled
@@ -52,13 +62,15 @@ module.exports = {
     // does not work with type definitions
     'no-unused-vars': 'off',
     // allow debugger during development only
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
   },
   // Overriding Vue3 eslint rules
-  overrides: [{
-    files: ['src/pages/**/*.vue'],
-    rules: {
-      'vue/multi-word-component-names': 0
-    }
-  }]
+  overrides: [
+    {
+      files: ['src/pages/**/*.vue'],
+      rules: {
+        'vue/multi-word-component-names': 0,
+      },
+    },
+  ],
 };
