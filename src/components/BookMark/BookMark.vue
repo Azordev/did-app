3a726934474635336005f51e70df31ea0604b8cd
Victor P <katsukeinz@gmail.com>
2022-06-09 01:20:02 -0400

    Added BookMark component

---
+++ src/components/BookMark/BookMark.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="BookMark">{{ label }}</div>
+</template>
+
+<script lang="ts">
+import './styles.scss';
+import { defineComponent } from 'vue';
+
+export default defineComponent({
+  name: 'BookMark',
+  props: {
+    label: {
+      type: String,
+      default: '',
+    },
+  },
+});
+</script>
