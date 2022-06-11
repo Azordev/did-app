195bbcc1fde081c6ff0ee6c2da3ca2c941fb7c02
Victor P <katsukeinz@gmail.com>
2022-06-10 20:15:31 -0400

    Some style improves

--- src/pages/Home/Home.layout.vue
+++ src/pages/Home/Home.layout.vue
@@ -9,35 +9,60 @@
   />
   <div class="HomeLayout__content">
     <home-list class="HomeLayout__list" title="Eventos">
-      <div v-for="event of events" :key="event.id">
-        <event-card
-          :title="event.title"
-          :image_url="event.image_url"
-          :date="new Date(event.date)"
-          :isAnnouncement="event.type === 'PUBLIC'"
-          @onClick="
-            $router.push({
-              name: 'eventDetail',
-              params: {
-                event: event.id,
-              },
-            })
-          "
-        />
+      <template v-if="isMembershipActive">
+        <div
+          class="HomeLayout__listItem"
+          v-for="event of events"
+          :key="event.id"
+        >
+          <event-card
+            :title="event.title"
+            :image_url="event.image_url"
+            :date="new Date(event.date)"
+            :isAnnouncement="event.type === 'PUBLIC'"
+            @onClick="
+              $router.push({
+                name: 'eventDetail',
+                params: {
+                  event: event.id,
+                },
+              })
+            "
+          />
+        </div>
+        <div class="HomeLayout__seeAll">
+          <router-link :to="{ name: 'events' }">Ver todos</router-link>
+        </div>
+      </template>
+      <div v-else>
+        Por favor actualice su membresia para ver los proximos eventos
+        disponibles en DID
       </div>
     </home-list>
     <home-list class="HomeLayout__list" title="Proveedores">
-      <div v-for="provider of providers" :key="provider.id">
-        <provider-card
-          @onClick="
-            $router.push({
-              name: 'providerDetail',
-              params: { provider: provider.id },
-            })
-          "
-          :image_url="provider.logo_url"
-          :id="provider.id"
-        />
+      <template v-if="isMembershipActive">
+        <div
+          class="HomeLayout__listItem"
+          v-for="provider of providers"
+          :key="provider.id"
+        >
+          <provider-card
+            @onClick="
+              $router.push({
+                name: 'providerDetail',
+                params: { provider: provider.id },
+              })
+            "
+            :image_url="provider.logo_url"
+            :id="provider.id"
+          />
+        </div>
+        <div class="HomeLayout__seeAll">
+          <router-link :to="{ name: 'providers' }">Ver todos</router-link>
+        </div>
+      </template>
+      <div v-else>
+        Por favor actualice su membresia para disfrutar de descuentos exclusivos
       </div>
     </home-list>
   </div>
