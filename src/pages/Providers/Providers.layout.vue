e98db2bfbc6ef1bf4aa44b03a2e4c17f802a1be7
Victor P <katsukeinz@gmail.com>
2022-06-21 17:25:55 -0400

    Refactored providers layout view

--- src/pages/Providers/Providers.layout.vue
+++ src/pages/Providers/Providers.layout.vue
@@ -10,11 +10,6 @@
         :query-value="queryValue"
         class="Providers__search"
       />
-      <providers-categories
-        :categorySelected="categorySelected"
-        :categories="categories"
-        @update:categorySelected="$emit('onSelectCategory', $event)"
-      />
 
       <list-grid
         className="ProvidersList"
@@ -37,53 +32,26 @@
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
+<script setup lang="ts">
 import ListGrid from '../../components/ListGrid';
 import BackButton from '../../components/BackButton';
 import SearchBar from '../../components/SearchBar';
 import ProvidersListItem from './ProvidersListItem.vue';
-import ProvidersCategories from '../../components/CategoriesSlider/CategoriesSlider.vue';
+import { Provider } from 'src/utils';
 
-export default defineComponent({
-  name: 'ProductsLayout',
-  components: {
-    ListGrid,
-    ProvidersListItem,
-    BackButton,
-    SearchBar,
-    ProvidersCategories,
-  },
-  emits: ['onSelectCategory', 'update:queryValue', 'onClear', 'onSearch'],
-  props: {
-    isLoading: {
-      type: Boolean,
-      default: false,
-    },
-    providers: {
-      type: Array,
-      default: () => [],
-    },
-    totalPages: {
-      type: Number,
-      default: 1,
-    },
-    currentPage: {
-      type: Number,
-      default: 1,
-    },
-    categories: {
-      type: Array,
-      default: () => [],
-    },
-    categorySelected: {
-      type: String,
-      default: '',
-    },
-    queryValue: {
-      type: String,
-      default: '',
-    },
-  },
-});
+interface ProvidersLayoutEmits {
+  (EventName: 'onSelectCategory', categorySelected: string): void;
+  (EventName: 'update:queryValue', queryValue: string): void;
+  (EventName: 'onSearch'): void;
+  (EventName: 'onClear'): void;
+}
+
+interface ProvidersLayoutProps {
+  isLoading: boolean;
+  providers: Provider[];
+  queryValue: string;
+}
+
+defineProps<ProvidersLayoutProps>();
+defineEmits<ProvidersLayoutEmits>();
 </script>
