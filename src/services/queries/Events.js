eb958b707e71094d7f98db5eb2ec03760ec1af62
Victor P <katsukeinz@gmail.com>
2022-06-29 18:16:34 -0400

    Using pascal case on fragments

--- src/services/queries/Events.js
+++ src/services/queries/Events.js
@@ -1,24 +1,24 @@
 import gql from 'graphql-tag';
-import { eventInfo } from '../fragments';
+import { EventInfo } from '../fragments';
 
 export const EVENTS = gql`
-  ${eventInfo}
+  ${EventInfo}
   query GetEventsForHome($limit: Int = 4, $name: String = "%%") {
     events(
       order_by: { date: asc }
       limit: $limit
       where: { is_active: { _eq: true }, title: { _ilike: $name } }
     ) {
-      ...eventsFragment
+      ...EventsFragment
     }
   }
 `;
 
 export const EVENT_BY_ID_QUERY = gql`
-  ${eventInfo}
+  ${EventInfo}
   query GetEventById($id: uuid!) {
     events_by_pk(id: $id) {
-      ...eventsFragment
+      ...EventsFragment
     }
   }
 `;
