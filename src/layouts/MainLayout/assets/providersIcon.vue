b5836df8e8ada4edbcdc73019cb32381fc06fb16
Victor P <katsukeinz@gmail.com>
2022-07-08 09:57:41 -0400

    Fixing layout icons

--- src/layouts/MainLayout/assets/providersIcon.vue
+++ src/layouts/MainLayout/assets/providersIcon.vue
@@ -1,48 +1,54 @@
 <template>
   <svg
     xmlns="http://www.w3.org/2000/svg"
-    width="13.387"
-    height="16.619"
-    viewBox="0 0 13.387 16.619"
+    width="24.792"
+    height="31"
+    viewBox="0 0 24.792 31"
   >
     <g
-      id="Grupo_11172"
-      data-name="Grupo 11172"
-      transform="translate(0.501 0.5)"
+      id="Grupo_13260"
+      data-name="Grupo 13260"
+      transform="translate(-28.358 0.5)"
     >
-      <g
-        id="shopping-bag_1_"
-        data-name="shopping-bag (1)"
-        transform="translate(0)"
-      >
-        <g id="Grupo_11169" data-name="Grupo 11169" transform="translate(0)">
-          <g id="Grupo_11168" data-name="Grupo 11168" transform="translate(0)">
-            <path
-              id="Trazado_31565"
-              data-name="Trazado 31565"
-              d="M65.4,12.88l-.7-8.789a1.1,1.1,0,0,0-1.09-1.01H62.231V3.019a3.019,3.019,0,1,0-6.038,0v.062H54.815a1.1,1.1,0,0,0-1.09,1.009l-.7,8.791a2.543,2.543,0,0,0,2.536,2.738h7.3A2.543,2.543,0,0,0,65.4,12.88ZM57.108,3.019a2.1,2.1,0,1,1,4.208,0v.062H57.108Zm6.948,11.163a1.612,1.612,0,0,1-1.195.522h-7.3a1.628,1.628,0,0,1-1.624-1.752l.7-8.792A.179.179,0,0,1,54.815,4h1.378V5.123a.457.457,0,1,0,.915,0V4h4.208V5.123a.457.457,0,1,0,.915,0V4h1.378a.18.18,0,0,1,.178.166l.7,8.789A1.612,1.612,0,0,1,64.056,14.182Z"
-              transform="translate(-53.02)"
-              fill="#919191"
-              stroke="#919191"
-              stroke-width="1"
-            />
-          </g>
-        </g>
+      <g id="Grupo_11172" data-name="Grupo 11172" transform="translate(28.861)">
         <g
-          id="Grupo_11171"
-          data-name="Grupo 11171"
-          transform="translate(3.997 7.623)"
+          id="shopping-bag_1_"
+          data-name="shopping-bag (1)"
+          transform="translate(0)"
         >
-          <g id="Grupo_11170" data-name="Grupo 11170" transform="translate(0)">
-            <path
-              id="Trazado_31566"
-              data-name="Trazado 31566"
-              d="M188.308,250.027a.457.457,0,0,0-.647,0l-2.043,2.043-.785-.785a.457.457,0,0,0-.647.647l1.109,1.109a.457.457,0,0,0,.647,0l2.366-2.366A.457.457,0,0,0,188.308,250.027Z"
-              transform="translate(-184.052 -249.893)"
-              fill="#919191"
-              stroke="#919191"
-              stroke-width="1"
-            />
+          <g id="Grupo_11169" data-name="Grupo 11169" transform="translate(0)">
+            <g id="Grupo_11168" data-name="Grupo 11168">
+              <path
+                id="Trazado_31565"
+                data-name="Trazado 31565"
+                d="M76.793,24.739,75.453,7.858a2.11,2.11,0,0,0-2.094-1.94H70.712V5.8a5.8,5.8,0,1,0-11.6,0v.119H56.468a2.109,2.109,0,0,0-2.094,1.937l-1.34,16.886A4.885,4.885,0,0,0,57.9,30H71.922a4.885,4.885,0,0,0,4.87-5.261ZM60.872,5.8a4.042,4.042,0,1,1,8.083,0v.119H60.872ZM74.217,27.241a3.1,3.1,0,0,1-2.295,1H57.9a3.127,3.127,0,0,1-3.119-3.364l1.34-16.886a.345.345,0,0,1,.342-.317h2.646V9.839a.879.879,0,1,0,1.757,0V7.675h8.083V9.839a.879.879,0,0,0,1.757,0V7.675h2.646a.345.345,0,0,1,.342.319l1.34,16.882A3.1,3.1,0,0,1,74.217,27.241Z"
+                transform="translate(-53.02)"
+                fill="#919191"
+                stroke="#c9c9c9"
+                stroke-width="1"
+              />
+            </g>
+          </g>
+          <g
+            id="Grupo_11171"
+            data-name="Grupo 11171"
+            transform="translate(7.678 14.642)"
+          >
+            <g
+              id="Grupo_11170"
+              data-name="Grupo 11170"
+              transform="translate(0)"
+            >
+              <path
+                id="Trazado_31566"
+                data-name="Trazado 31566"
+                d="M192.226,250.15a.878.878,0,0,0-1.242,0l-3.923,3.923-1.509-1.509a.879.879,0,0,0-1.243,1.243l2.13,2.13a.878.878,0,0,0,1.242,0l4.544-4.544A.879.879,0,0,0,192.226,250.15Z"
+                transform="translate(-184.052 -249.893)"
+                fill="#919191"
+                stroke="#c9c9c9"
+                stroke-width="1"
+              />
+            </g>
           </g>
         </g>
       </g>
@@ -50,8 +56,4 @@
   </svg>
 </template>
 
-<script>
-export default {
-  name: 'providersIcon',
-};
-</script>
+<script setup></script>
