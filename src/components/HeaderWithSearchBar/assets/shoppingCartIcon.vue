f6371cea0b1712598e5878468fc9138321b1de99
Victor P <katsukeinz@gmail.com>
2022-07-09 20:22:34 -0400

    Fixed Provider view header and product container

---
+++ src/components/HeaderWithSearchBar/assets/shoppingCartIcon.vue
@@ -0,0 +1,67 @@
+<template>
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    :width="width"
+    :height="height"
+    viewBox="0 0 24.792 31"
+    fill="currentColor"
+  >
+    <g
+      id="Grupo_13260"
+      data-name="Grupo 13260"
+      transform="translate(-28.358 0.5)"
+    >
+      <g id="Grupo_11172" data-name="Grupo 11172" transform="translate(28.861)">
+        <g
+          id="shopping-bag_1_"
+          data-name="shopping-bag (1)"
+          transform="translate(0)"
+        >
+          <g id="Grupo_11169" data-name="Grupo 11169" transform="translate(0)">
+            <g id="Grupo_11168" data-name="Grupo 11168">
+              <path
+                id="Trazado_31565"
+                data-name="Trazado 31565"
+                d="M76.793,24.739,75.453,7.858a2.11,2.11,0,0,0-2.094-1.94H70.712V5.8a5.8,5.8,0,1,0-11.6,0v.119H56.468a2.109,2.109,0,0,0-2.094,1.937l-1.34,16.886A4.885,4.885,0,0,0,57.9,30H71.922a4.885,4.885,0,0,0,4.87-5.261ZM60.872,5.8a4.042,4.042,0,1,1,8.083,0v.119H60.872ZM74.217,27.241a3.1,3.1,0,0,1-2.295,1H57.9a3.127,3.127,0,0,1-3.119-3.364l1.34-16.886a.345.345,0,0,1,.342-.317h2.646V9.839a.879.879,0,1,0,1.757,0V7.675h8.083V9.839a.879.879,0,0,0,1.757,0V7.675h2.646a.345.345,0,0,1,.342.319l1.34,16.882A3.1,3.1,0,0,1,74.217,27.241Z"
+                transform="translate(-53.02)"
+                stroke-width="1"
+              />
+            </g>
+          </g>
+          <g
+            id="Grupo_11171"
+            data-name="Grupo 11171"
+            transform="translate(7.678 14.642)"
+          >
+            <g
+              id="Grupo_11170"
+              data-name="Grupo 11170"
+              transform="translate(0)"
+            >
+              <path
+                id="Trazado_31566"
+                data-name="Trazado 31566"
+                d="M192.226,250.15a.878.878,0,0,0-1.242,0l-3.923,3.923-1.509-1.509a.879.879,0,0,0-1.243,1.243l2.13,2.13a.878.878,0,0,0,1.242,0l4.544-4.544A.879.879,0,0,0,192.226,250.15Z"
+                transform="translate(-184.052 -249.893)"
+                stroke-width="1"
+              />
+            </g>
+          </g>
+        </g>
+      </g>
+    </g>
+  </svg>
+</template>
+
+<script setup lang="ts">
+defineProps({
+  width: {
+    type: Number,
+    default: 24.792,
+  },
+  height: {
+    type: Number,
+    default: 31,
+  },
+});
+</script>
