990f6c00a322c91f21eba2c8c20d061a32de6e6d
Victor P <katsukeinz@gmail.com>
2022-07-31 14:20:39 -0400

    improving user code

--- src/pages/User/index.vue
+++ src/pages/User/index.vue
@@ -2,10 +2,4 @@
   <div>User Page</div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from 'vue';
-
-export default defineComponent({
-  name: 'User',
-});
-</script>
+<script setup lang="ts"></script>
