eafac4a1fa5d35c8e7d10f286463d8db13e58296
Victor P <katsukeinz@gmail.com>
2022-07-31 14:27:10 -0400

    Improve Bookmark code

--- src/components/BookMark/BookMark.vue
+++ src/components/BookMark/BookMark.vue
@@ -2,17 +2,12 @@
   <div class="BookMark">{{ label }}</div>
 </template>
 
-<script lang="ts">
+<script setup lang="ts">
 import './styles.scss';
-import { defineComponent } from 'vue';
 
-export default defineComponent({
-  name: 'BookMark',
-  props: {
-    label: {
-      type: String,
-      default: '',
-    },
-  },
-});
+interface BookMarkProps {
+  label: string;
+}
+
+defineProps<BookMarkProps>();
 </script>
